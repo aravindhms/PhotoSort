1f92ff791311927174b0c545fe26fb3ed761d6f8
Aravindh <aravindhms1@gmail.com>
2020-07-16 16:49:25 +0530

    Initial

--- rename.py
+++ rename.py
@@ -8,5 +8,5 @@ entries = os.listdir(source)
 for entry in entries:
     if os.path.isfile(source+entry):
         if r' (1)' in entry:
-            newname=entry[0:8]+".NEF"
+            newname = entry[0:8]+".NEF"
             shutil.move(source + entry, source + newname)
