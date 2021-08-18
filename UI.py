5a56369f6932ee78a5a5ab7a28293213cf05992d
Aravindh <aravindhms1@gmail.com>
2021-08-18 11:51:40 +0530

    Implemented folder structure selection

--- UI.py
+++ UI.py
@@ -25,13 +25,24 @@ Status = [
     [sg.Text(key="STATUS", size=(50, 1), enable_events=True)]
 ]
 
+info_panel = [
+    [
+        sg.Text("Supported file formats : JPEG, JPG, PNG, RAW, NEF, CR2"),
+
+    ]
+]
+
 layout = [
+
     [
         sg.Column(source_folder_input)
     ],
     [
         sg.Column(destination_folder_input),
     ],
+
+    [sg.Text('Select folder structure')],
+    [sg.Radio('Year/Month', 'fold_struct', size=(12, 1), key="YM",default=True), sg.Radio('Year/Month/Day', 'fold_struct',key="YMD", size=(12, 1))],
     [
         sg.Column(Status),
     ],
@@ -39,7 +50,10 @@ layout = [
         sg.Button("Move", enable_events=True, key="MOVE", button_color=(sg.YELLOWS[0], sg.BLUES[0])),
         sg.Button("Copy", enable_events=True, key="COPY", button_color=(sg.YELLOWS[0], sg.BLUES[0]))
     ],
-    [sg.ProgressBar(1000, orientation='h', size=(20, 20), key='progressbar')]
+    [
+        sg.StatusBar("Supported file formats : JPEG, JPG, PNG, RAW, NEF")
+    ]
+    # [sg.ProgressBar(1000, orientation='h', size=(20, 20), key='progressbar')]
 
 ]
 
@@ -64,17 +78,21 @@ while True:
     if event == "DESTINATION":
         destination_folder = values["DESTINATION"]
         if source_folder == destination_folder:
-            window["STATUS"].update("Warning : Source and destination are same!!!")
+            window["STATUS"].update("Warning : Source and destination are same!!!", text_color='red',background_color='yellow')
+    if values["YM"]   == True:
+        folder_struct = 'YM'
+    if values["YMD"]   == True:
+        folder_struct = 'YMD'
 
     if event == "MOVE":
         window["STATUS"].update("Processing....")
         process_type = 'move'
-        sort_count, error_count = photosort.sort_photos(source_folder+"/", destination_folder, process_type)
+        sort_count, error_count = photosort.sort_photos(source_folder+"/", destination_folder, process_type,folder_struct)
         window["STATUS"].update(str(sort_count) + " photo(s) moved. "+str(error_count) + " Error(s) ")
 
     if event == "COPY":
         window["STATUS"].update("Processing....")
         process_type = 'copy'
         print(source_folder+"/", destination_folder, process_type)
-        sort_count, error_count = photosort.sort_photos(source_folder+"/", destination_folder, process_type)
+        sort_count, error_count = photosort.sort_photos(source_folder+"/", destination_folder, process_type,folder_struct)
         window["STATUS"].update(str(sort_count) + " photo(s) copied. "+str(error_count) + " Error(s) ")
--- photosort.py
+++ photosort.py
@@ -5,26 +5,30 @@ import exifread
 
 # source = r"E:/Nikon/Unsorted/"
 # destination = r"E:/Nikon/Sorted"
-image_formats = ["JPEG", "JPG", "PNG", "RAW", "NEF"]
+image_formats = ["JPEG", "JPG", "PNG", "RAW", "NEF","CR2"]
 # process_type = 'copy'
 
 
-def sort_photos(source_folder, destination_folder, process_type):
+def sort_photos(source_folder, destination_folder, process_type,folder_struct):
     process_count = 0
     error_count = 0
+    print(folder_struct)
     entries = os.listdir(source_folder)
     for entry in entries:
         if entry.upper().endswith(tuple(image_formats)):
             try:
                 f = open(source_folder + entry, 'rb')
-                print(entry)
                 tags = exifread.process_file(f, details=False)
                 f.close()
                 photo_date = tags["EXIF DateTimeOriginal"]
                 year = str(photo_date)[0:4]
                 month_int = int(str(photo_date)[5:7])
                 month = datetime.date(1900, month_int, 1).strftime('%B')
-                folder = destination_folder + "/" + year + "/" + month + "/"
+                day = str(photo_date)[8:10]
+                if folder_struct == 'YM':
+                    folder = destination_folder + "/" + year + "/" + month + "/"
+                if folder_struct == 'YMD':
+                    folder = destination_folder + "/" + year + "/" + month + "/"+ day + "/"
                 if not os.path.exists(folder):
                     os.makedirs(folder)
                 if process_type == 'move':
